5f501f9cd9e5240141f33bc297825745508cc1bf
Shteab <42419453+Shteab@users.noreply.github.com>
2019-12-24 08:02:55 -0700

    Completed module

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -5,16 +5,16 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace StarChart.Data
+namespace StarChart.Models
 {
     public class CelestialObject
     {
         public int Id { get; set; }
-
+        
         [Required]
         public string Name { get; set; }
-
         public int? OrbitedObjectId { get; set; }
+
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
